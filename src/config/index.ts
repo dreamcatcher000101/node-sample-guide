9066aeb0eec5dd67d416ec863f60c87d46e36788
Mr Dev <mrdev0803@gmail.com>
2022-06-11 09:36:28 -0700

    feat: create user model

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,4 @@
 export const ROUTE_VERSION: string = "v1";
+
+export const JWT_SECRET: string = "jsonwebtoken";
+export const JWT_EXPIRE: string = "1h";
--- src/models/user.model/index.ts
+++ src/models/user.model/index.ts
@@ -1,19 +1,83 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import { model, Schema, Model, Document } from "mongoose";
 
-interface IUser extends Document {
-  fullName: string;
+import { JWT_SECRET, JWT_EXPIRE } from "../../config";
+
+interface IUser {
+  fullname?: string;
   email: string;
   password: string;
 }
 
-const UserSchema: Schema = new Schema<IUser>({
-  fullName: { type: String, required: true },
+interface IUserAuthJSON {
+  fullname: string;
+  email: string;
+}
+
+interface IUserModel extends Document, IUser {
+  createdAt: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+  setPassword(password: string): void;
+  validatePassword(password: string): void;
+  generateJWT(): string;
+  toAuthJSON(): IUserAuthJSON;
+}
+
+const UserSchema: Schema = new Schema<IUserModel>({
+  fullname: { type: String, required: true, minlength: 6 },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
+  createdAt: { type: Date, required: true },
+  updatedAt: { type: Date },
+  deletedAt: { type: Date },
 });
 
-const UserModel: Model<IUser> = model("User", UserSchema);
+UserSchema.pre("save", function (next) {
+  if (this.createdAt) {
+    this.updatedAt = new Date();
+  } else {
+    this.createdAt = new Date();
+  }
 
-export { IUser };
+  next();
+});
+
+UserSchema.methods.setPassword = function (password: string) {
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  this.password = hashedPassword;
+};
+
+UserSchema.methods.validatePassword = function (password: string): boolean {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.generateJWT = function (): string {
+  return jwt.sign(
+    {
+      id: this._id,
+      fullname: this.fullname,
+      email: this.email,
+    },
+    JWT_SECRET,
+    {
+      expiresIn: JWT_EXPIRE,
+    }
+  );
+};
+
+UserSchema.methods.toAuthJSON = function () {
+  return {
+    fullname: this.fullname,
+    email: this.email,
+    token: this.generateJWT(),
+  };
+};
+
+const UserModel: Model<IUserModel> = model("User", UserSchema);
+
+export { IUser, IUserAuthJSON };
 
 export default UserModel;
