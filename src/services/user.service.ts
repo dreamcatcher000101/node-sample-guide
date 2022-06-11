c34689a6c99d5b66709e1c53090f7c5187de9522
Mr Dev <mrdev0803@gmail.com>
2022-06-10 17:34:53 -0700

    feat: read filtered users in service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,16 @@
+import { UserModel } from "../models";
+
 export const createUser = async () => {};
 
-export const readUsers = async () => {};
+export const readUsers = async (userData: any) => {
+  return await UserModel.find(
+    {
+      deletedAt: null,
+      ...userData,
+    },
+    { password: 0 }
+  );
+};
 
 export const updateUser = async () => {};
 
